175b5db8d13e1dd4f172140a43e3dff357691e44
Matt Cameron <cameron.matt@gmail.com>
2018-05-24 10:58:02 -0500

    impl filtering

--- src/components/CarListing.js
+++ src/components/CarListing.js
@@ -2,32 +2,62 @@ import React from "react";
 import PropTypes from "prop-types";
 
 function CarListing(props) {
-    return (<div className="col-md-4 grid_listing">
-            <div className="product-listing-m gray-bg">
-              <div className="product-listing-img"> <a href="http://themes.webmasterdriver.net/carforyou/demo/listing-grid.html#">
-              <img src="./featured-img-1.jpg" className="img-responsive" alt="image" /> </a>
-                <div className="label_icon">New</div>
-                <div className="compare_item">
-                  <div className="checkbox">
-                    <input type="checkbox" value="" id="compare10" />
-                    <label for="compare10">Compare</label>
-                  </div>
-                </div>
-              </div>
-              <div className="product-listing-content">
-                <h5><a href="http://themes.webmasterdriver.net/carforyou/demo/listing-grid.html#">Mazda CX-5 SX, V6, ABS, Sunroof </a></h5>
-                <p className="list-price">$89,000</p>
-                <div className="car-location"><span><i className="fa fa-map-marker" aria-hidden="true"></i> Colorado, USA</span></div>
-                <ul className="features_list">
-                  <li><i className="fa fa-road" aria-hidden="true"></i>35,000 km</li>
-                  <li><i className="fa fa-tachometer" aria-hidden="true"></i>30.000 miles</li>
-                  <li><i className="fa fa-calendar" aria-hidden="true"></i>2005 model</li>
-                  <li><i className="fa fa-car" aria-hidden="true"></i>Diesel</li>
-                </ul>
-              </div>
+  const { car } = props;
+  console.log(props);
+  return (
+    <div className="col-md-4 grid_listing">
+      <div className="product-listing-m gray-bg">
+        <div className="product-listing-img">
+          {" "}
+          <a href="http://themes.webmasterdriver.net/carforyou/demo/listing-grid.html#">
+            <img
+              src="./featured-img-1.jpg"
+              className="img-responsive"
+              alt="image"
+            />{" "}
+          </a>
+          <div className="label_icon">{ car.isNew ? "New" : "Pre-Owned" }</div>
+          <div className="compare_item">
+            <div className="checkbox">
+              <input type="checkbox" value="" id="compare10" />
+              <label htmlFor="compare10">Compare</label>
             </div>
-          </div>);
+          </div>
+        </div>
+        <div className="product-listing-content">
+          <h5>
+            <a href="http://themes.webmasterdriver.net/carforyou/demo/listing-grid.html#">
+              {car.make} {car.model}{" "}
+            </a>
+          </h5>
+          <p className="list-price">{car.price}</p>
+          <div className="car-location">
+            <span>
+              <i className="fa fa-map-marker" aria-hidden="true" /> {car.city}
+            </span>
+          </div>
+          <ul className="features_list">
+            <li>
+              <i className="fa fa-road" aria-hidden="true" />{car.km} km
+            </li>
+            <li>
+              <i className="fa fa-tachometer" aria-hidden="true" />{car.miles} miles
+            </li>
+            <li>
+              <i className="fa fa-calendar" aria-hidden="true" />{car.year} model
+            </li>
+            <li>
+              <i className="fa fa-car" aria-hidden="true" />{car.fuel}
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default CarListing;
+// CarListing.propTypes = {
+//   car: PropTypes.object.isRequired,
+// };
 
+export default CarListing;
